443cba7bf1e1c21e2bbd4dd710857ac5996e450b
Jonas Mueller <jonas.mueller@gmx.at>
2024-07-17 21:53:05 +0200

    Add arrow keys to get to next/prev image

--- main.go
+++ main.go
@@ -16,7 +16,8 @@ const BOX_SPACING int = 2
 
 const LABEL_MARGIN = 10
 
-var RAW_SUFFIXES = []string{"cr3", "cr2", "dng"}
+var RAW_SUFFIXES = []string{"cr3", "cr2", "dng", "360", "3fr", "3g2", "3gp", "3gp2", "3gpp", "7z", "a", "aa", "aae", "aax", "acfm", "acr", "afm", "ai", "aif", "aifc", "aiff", "ait", "amfm", "ape", "apng", "arq", "arw", "asf", "avi", "avif", "azw", "azw3", "bmp", "bpg", "btf", "chm", "ciff", "cos", "cr2", "cr3", "crm", "crw", "cs1", "csv", "cur", "czi", "dc3", "dcm", "dcp", "dcr", "dfont", "dib", "dic", "dicm", "divx", "djv", "djvu", "dll", "dng", "doc", "docm", "docx", "dot", "dotm", "dotx", "dpx", "dr4", "ds2", "dss", "dv", "dvb", "dvr-ms", "dylib", "eip", "eps", "eps2", "eps3", "epsf", "epub", "erf", "exe", "exif", "exr", "exv", "f4a", "f4b", "f4p", "f4v", "fff", "fit", "fits", "fla", "flac", "flif", "flir", "flv", "fpf", "fpx", "gif", "glv", "gpr", "gz", "gzip", "hdp", "hdr", "heic", "heif", "hif", "htm", "html", "ical", "icc", "icm", "ico", "ics", "idml", "iiq", "ind", "indd", "indt", "insp", "insv", "inx", "iso", "itc", "j2c", "j2k", "jng", "jp2", "jpc", "jpe", "jpf", "jpm", "jps", "jpx", "json", "jxl", "jxr", "k25", "kdc", "key", "kth", "la", "lfp", "lfr", "lif", "lnk", "lrv", "m2t", "m2ts", "m2v", "m4a", "m4b", "m4p", "m4v", "macos", "max", "mef", "mie", "mif", "miff", "mka", "mks", "mkv", "mng", "mobi", "modd", "moi", "mos", "mov", "mp3", "mp4", "mpc", "mpeg", "mpg", "mpo", "mqv", "mrc", "mrw", "mts", "mxf", "nef", "newer", "nksc", "nmbtemplate", "nrw", "numbers", "o", "odb", "odc", "odf", "odg", "odi", "odp", "ods", "odt", "ofr", "ogg", "ogv", "onp", "opus", "orf", "ori", "otf", "pac", "pages", "pbm", "pcd", "pct", "pcx", "pdb", "pdf", "pef", "pfa", "pfb", "pfm", "pgf", "pgm", "pict", "plist", "pmp", "pot", "potm", "potx", "ppam", "ppax", "ppm", "pps", "ppsm", "ppsx", "ppt", "pptm", "pptx", "prc", "ps", "ps2", "ps3", "psb", "psd", "psdt", "psp", "pspframe", "pspimage", "pspshape", "psptube", "qif", "qt", "qti", "qtif", "r3d", "ra", "raf", "ram", "rar", "raw", "rif", "riff", "rm", "rmvb", "rpm", "rsrc", "rtf", "rv", "rw2", "rwl", "rwz", "seq", "sketch", "so", "sr2", "srf", "srw", "svg", "swf", "thm", "thmx", "tif", "tiff", "torrent", "ts", "ttc", "ttf", "tub", "txt", "vcard", "vcf", "vnt", "vob", "vrd", "vsd", "wav", "wdp", "webm", "webp", "wma", "wmv", "woff", "woff2", "wpg", "wtv", "wv", "x3f", "xcf", "xhtml", "xla", "xlam", "xls", "xlsb", "xlsm", "xlsx", "xlt", "xltm", "xltx", "xmp", "zip"}
+
 var DISPLAY_SUFFIXES = []string{"jpg", "jpeg", "png"}
 
 var (
@@ -255,8 +256,43 @@ func keyPress(win *gtk.ApplicationWindow, event *gdk.Event) {
 		changeRating(4)
 	case gdk.KEY_5:
 		changeRating(5)
+	case gdk.KEY_Left:
+		prevImage()
+	case gdk.KEY_Right:
+		nextImage()
+	case gdk.KEY_Up:
+		prevImage()
+	case gdk.KEY_Down:
+		nextImage()
+	default:
+		fmt.Println("Key pressed:", keyVal)
 	}
 	win.ShowAll()
+}
+
+func nextImage() {
+	// change to the next image
+	incrementImageBy(1)
+}
+
+func prevImage() {
+	// change to the previous image
+	incrementImageBy(-1)
+}
+
+func incrementImageBy(increment int) {
+	selectedRow := listView.listBox.GetSelectedRow()
+	if selectedRow != nil {
+		selectedIndex := selectedRow.GetIndex()
+		nextIndex := selectedIndex + increment
+		if nextIndex >= 0 && nextIndex < int(listView.listBox.GetChildren().Length()) {
+			listView.listBox.SelectRow(listView.listBox.GetRowAtIndex(nextIndex))
+		}
+	} else if listView.listBox.GetChildren().Length() > 0 {
+		// if there is no selected row, select the first row
+		firstRow := listView.listBox.GetRowAtIndex(0)
+		listView.listBox.SelectRow(firstRow)
+	}
 }
 
 func changeRating(rating int) {
